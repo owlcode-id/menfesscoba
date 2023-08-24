11e2315242c3d497c2576f9b4a9d18943e24d42c
owlcode-id <127479298+owlcode-id@users.noreply.github.com>
2023-08-24 11:14:37 +0700

    Update menfess.py

--- menfess.py
+++ menfess.py
@@ -17,7 +17,7 @@ try:
     from dotenv import load_dotenv
     from telebot import types
 except:
-    print("error! install dulu pytelegrambotapi dengan cara 'pip install pytelegrambotapi'")
+    print("error! install pytelegrambotapi first by running 'pip install pytelegrambotapi'")
 
 load_dotenv()
 
@@ -56,31 +56,28 @@ def diam(id):
 def garz(message):
     id = message.chat.id
     teks = message.text
-
-    with open("member.db", "a+") as file:
-        file.seek(0)
-        value = str(id)
-        lines = file.read().splitlines()
-        if value in lines:
-            pass
-        else:
-            file.write(value + "\n")
-
-    if "/start" in teks:
-        nggih = '\n'.join(map(str, trigger))
-        yamete = ma(row_width=2)
-        rawr = bb(text="Channel Menfess", url=link)
-        rawr_gc = bb(text="Grup Menfess", url=f"https://t.me/joinchat/{gc}")
-        yamete.add(rawr, rawr_gc)
-        kirim(id, mulai.format(nggih), parse_mode="markdown", reply_markup=yamete)
-    elif "/ping" in teks:
-        total = len(open("member.db", "r").readlines())
-        pong = f"Bot Aktif !!!!\nTotal Pengguna Bot : {total}"
-        kirim(id, pong)
-    elif "/broadcast" in teks:
-        if id in admin:
-            anjim = kirim(id, "Masukan Pesan Broadcast : ")
-            lanjut(anjim, broadcast)
+    
+    is_joined_channel = bot.get_chat_member(ch, id).status == "member"
+    is_joined_group = bot.get_chat_member(gc, id).status == "member"
+    
+    if not is_joined_channel or not is_joined_group:
+        kirim(id, "To use this bot, you must first join our channel and group.")
+        return
+    
+    nggih = '\n'.join(map(str, trigger))
+    yamete = ma(row_width=2)
+    rawr = bb(text="Channel Menfess", url=link)
+    
+    try:
+        group_invite_link = bot.export_chat_invite_link(gc)
+        rawr_gc = bb(text="Group Menfess", url=group_invite_link)
+    except Exception as e:
+        print(f"Error getting group invite link: {e}")
+        rawr_gc = bb(text="Group Menfess", url=f"https://t.me/c/{gc[4:]}")  # Fallback link
+        
+    yamete.add(rawr, rawr_gc)
+    kirim(id, mulai.format(nggih), parse_mode="markdown", reply_markup=yamete)
+    return
 
 @bot.message_handler(content_types=["text"])
 def menfessin(message):
@@ -88,19 +85,31 @@ def menfessin(message):
     teks = message.text
     ah = tegar(teks)
     ih = len(teks.split(" "))
+    
+    is_joined_channel = bot.get_chat_member(ch, id).status == "member"
+    is_joined_group = bot.get_chat_member(gc, id).status == "member"
+    
+    if not is_joined_channel or not is_joined_group:
+        kirim(id, "You must first join our channel and group before sending a menfess message.")
+        return
+    
     if id in apaantuh:
-        kirim(id, f"gagal mengirim!!\n\nkamu baru saja mengirim menfess, beri jarak {delay} detik untuk memposting kembali!")
+        kirim(id, f"Failed to send!!\n\nYou just sent a menfess, wait {delay} seconds before posting again!")
+        return
     elif ih < 3:
-        kirim(id, "gagal mengirim!!\n\ntidak boleh kurang dari 3 kata!!")
+        kirim(id, "Failed to send!!\n\nMessage must be at least 3 words long!!")
+        return
     elif ah == False:
         tag = '\n'.join(map(str, trigger))
-        kirim(id, f"gagal mengirim!!\n\nharap gunakan tag dibawah ini : \n{tag}")
+        kirim(id, f"Failed to send!!\n\nPlease use the tags below:\n{tag}")
+        return
     elif ah == True:
         pesan = kirim(ch, teks)
         links = link + "/" + str(pesan.id)
         linksk = links + "?comment=" + str(pesan.id)
-        kirim(id, f"*Menfess Berhasil Diposting!!*", parse_mode="markdown", reply_markup=awikwokbanget(links, linksk))
+        kirim(id, f"*Menfess Sent Successfully!!*", parse_mode="markdown", reply_markup=awikwokbanget(links, linksk))
         diam(id)
+        return
 
 def tegar(data):
     for x in data.split(" "):
@@ -124,15 +133,15 @@ def broadcast(message):
                 yy = int(x)
                 kopi(yy, id, pesans)
             except:
-                print(f"gagal mengirim pesan kepada pengguna *{x}*\nmungkin bot telah diblok.")
-    kirim(id, "Pesan broadcast berhasil dikirim.")
+                print(f"Failed to send a message to user *{x}*\nMaybe the bot has been blocked.")
+    kirim(id, "Broadcast message sent successfully.")
 
 def awikwokbanget(cek, cekin):
     miaw = ma(row_width=2)
-    b1 = bb(text="Cek Postingan", url=cek)
-    b2 = bb(text="Cek Komentar", url=cekin)
+    b1 = bb(text="Check Post", url=cek)
+    b2 = bb(text="Check Comment", url=cekin)
     miaw.add(b1, b2)
     return miaw
 
-print("\n\nBOT TELAH AKTIF!!! @GARZPROJECT")
+print("\n\nBOT IS ACTIVE!!! @GARZPROJECT")
 bot.infinity_polling()
